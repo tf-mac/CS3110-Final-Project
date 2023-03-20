f3527615d3072b7d392c4b7da4e876fe9e4739d3
Thomas Francis McFarland <tfm62@cornell.edu>
2023-03-20 19:25:01 -0400

    Implement significant additions
    - Modifying backing of the entries to be more efficient
    - Store types as the first row as well as labels
    - Create a type checking function

--- lib/readin.mli
+++ lib/readin.mli
@@ -1,12 +1,13 @@
 type input = Empty | Malformed | Valid of string list
+
 val user_defined_types : string list ref
 val lst_to_string : string list -> string
 val check_value_defn : string list -> input
 val parse_value_defn : string -> input
 val add_type : string -> unit
-val print_state : string -> 'a -> 'a
+val print_state : string -> string
 val read_value_defn : string -> string list list
 val parse_constructor_defn : string list -> string list list
 val read_make : 'a -> 'b list list
 val read_input : string -> string list list
-val main : unit -> string -> string list list
+val main : unit -> unit
