c107f5e32b4af3449269d0b360c95e91aee1d2d8
rsp224 <118997996+rsp224@users.noreply.github.com>
2023-05-12 16:53:44 -0400

    Document utils.mli

--- lib/utils.mli
+++ lib/utils.mli
@@ -9,9 +9,20 @@ type entry =
   | Id of (string * entry)
   | Type of (string * types)
 
+(*[name_map_entry t] returns a string representing the corresponding data type [t]*)
 val name_map_entry : entry -> string
+
+(* [name_map_types t] maps each [t] to a string.*)
 val name_map_types : types -> string
+
+(*[entry_to_string entry] converts [entry] into a string*)
 val entry_to_string : entry -> string
+
+(* [shorten inp] shortens [inp] to a maximum of 16 characters*)
 val shorten : string -> string
+
+(* [build_row entlist] converts [entlist] into a string, using shorten to limit the length of each entry.*)
 val build_row : entry option list -> string
+
+(* [optionize] wraps them in Some constructors*)
 val optionize : entry list -> entry option list
