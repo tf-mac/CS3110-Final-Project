96dff7cd907f18db8bf8bab4848b9e0c8318055e
rsp224 <118997996+rsp224@users.noreply.github.com>
2023-05-14 20:38:52 -0400

    Changed to raj's test suite
    Need to fix test cases for Table.insert, insert_named, Database.add_entry  and to strings (if necessary)

--- test/main.ml
+++ test/main.ml
@@ -1,90 +1,267 @@
 open OUnit2
-open RelationalDatabase.Cli
+open RelationalDatabase.Tables
+open RelationalDatabase.Database
+open RelationalDatabase.Utils
 
-module type StringList = sig
-  val input_list : string list ref
+module TableTests (T : Table) = struct
+  (*Table Tests*)
+
+  let test_empty_table name ex expected =
+    name >:: fun _ -> assert_equal expected (T.empty ex)
+
+  let test_insert name table elist expected =
+    name >:: fun _ ->
+    assert_equal expected (T.insert table elist) ~printer:T.table_to_string
+
+  let test_insert_exception name table elist expected =
+    name >:: fun _ -> assert_raises expected (fun () -> T.insert table elist)
+
+  let test_insert_named name table elist expected =
+    name >:: fun _ -> assert_equal expected (T.insert_named table elist)
+
+  let test_insert_named_exception name table elist expected =
+    name >:: fun _ ->
+    assert_raises expected (fun () -> T.insert_named table elist)
+
+  let test_at name table id expected =
+    name >:: fun _ -> assert_equal expected (T.at table id)
+
+  let test_at_exception name table id expected =
+    name >:: fun _ -> assert_raises expected (fun () -> T.at table id)
+
+  let test_delete name table id expected =
+    name >:: fun _ -> assert_equal expected (T.delete table id)
+
+  let test_table_to_string name table expected =
+    name >:: fun _ -> assert_equal expected (T.table_to_string table)
+
+  let test_exists_exception name table table_name expected =
+    name >:: fun _ ->
+    assert_raises expected (fun () -> T.exists table table_name)
+
+  let test_exists name table table_name expected =
+    name >:: fun _ -> assert_equal expected (T.exists table table_name)
+
+  let empty_table = T.empty []
+  let table = T.empty [ Type ("name", Strings); Type ("age", Ints) ]
+
+  let large_table =
+    T.empty
+      [
+        Type ("String", Strings);
+        Type ("Floats", Floats);
+        Type ("Ints", Ints);
+        Type ("Bools", Bools);
+        Type ("Chars", Chars);
+        Type ("Ids", Ids);
+      ]
+
+  let insert =
+    T.insert large_table
+      [
+        String "John";
+        Float 2.5;
+        Int 9;
+        Bool true;
+        Char 'a';
+        Id ("Person", String "Johnathan");
+      ]
+
+  let elist = [ ("name", String "John") ]
+  let elist2 = [ ("age", Int 25) ]
+  let elist_exception = [ ("", String "s") ]
+  let elist_exception2 = [ ("name", String "John") ]
+  let elist_for_at = [ Some (String "John") ]
+  let insert_named = T.insert_named table elist
+  let at_f = T.delete table (String "name")
+  let x = T.table_to_string at_f
+  let partial_table = T.empty [ Type ("name", Strings) ]
+  let partial_insert = T.insert_named partial_table elist
+  let test_table_d fun_name d = fun_name >:: fun _ -> print_endline x
+
+  let table_tests =
+    [
+      test_empty_table "empty table test" [] empty_table;
+      test_insert_named "add entry on existing table" table elist insert_named;
+      test_insert_named_exception "TypeMismatch thrown on insert_named" table
+        elist_exception TypeMismatch;
+      test_insert_named_exception "IndexExists thrown on insert_named"
+        insert_named elist_exception2 IndexExists;
+      test_insert "testing insert" large_table
+        [
+          String "John";
+          Float 2.5;
+          Int 9;
+          Bool true;
+          Char 'a';
+          Id ("Person", String "Johnathan");
+        ]
+        insert;
+      test_at "testing at for simple table" partial_insert (String "John")
+        elist_for_at;
+      test_at_exception "testing invalid string for at" insert_named
+        (String "invalid") Not_found;
+      test_at_exception "testing invalid type for at" insert_named (Int 25)
+        Not_found;
+      test_at_exception "testing at for an empty table" empty_table
+        (String "name") Not_found;
+      test_delete "testing invalid delete" partial_table (String "invalid")
+        partial_table;
+      test_delete "testing empty delete" empty_table (String "invalid")
+        empty_table;
+      test_exists "testing exists on existing table" table "name" Strings;
+      test_exists_exception "testing invalid exists on table" table "i"
+        TypeMismatch;
+      test_table_d "string 2" x;
+    ]
 end
 
-let better_print s = "'" ^ String.escaped s ^ "'"
-let get_response = CLI.get_response
+module ListTableTests = TableTests (ListTable)
+module HashTableTests = TableTests (HashTable)
 
-let rec add_input lst =
-  match lst with
-  | [] -> ()
-  | h :: t ->
-      h |> CLI.parse_input |> ignore;
-      add_input t
+module DatabaseTests (T : Table) = struct
+  module PersonDB = Database (T)
 
-let cli_test (name : string) (actual_output : string) (expected_output : string)
-    : test =
-  name >:: fun _ ->
-  assert_equal expected_output actual_output ~printer:better_print
+  let test_empty_database name expected =
+    name >:: fun _ -> assert_equal expected PersonDB.empty
 
-let rec make_test primer tests =
-  match tests with
-  | [] -> []
-  | (reset, name, input, expected) :: t ->
-      let placeholder =
-        if reset then (
-          CLI.reset ();
-          add_input primer)
-        else ();
-        cli_test name (CLI.parse_input input) expected
-      in
-      placeholder :: make_test primer t
+  let test_add_table fun_name database table name expected =
+    fun_name >:: fun _ ->
+    assert_equal (PersonDB.add_table database table name) expected
 
-let def_tests =
-  ( [],
+  let test_add_table_exception fun_name database table name expected =
+    fun_name >:: fun _ ->
+    assert_raises expected (fun () -> PersonDB.add_table database table name)
+
+  let test_build_table fun_name database table name expected =
+    fun_name >:: fun _ ->
+    assert_equal (PersonDB.build_table database table name) expected
+
+  let test_build_table_exception fun_name database table name expected =
+    fun_name >:: fun _ ->
+    assert_raises expected (fun () -> PersonDB.build_table database table name)
+
+  let test_drop_table fun_name name database expected =
+    fun_name >:: fun _ ->
+    assert_equal (PersonDB.drop_table name database) expected
+
+  let test_get_table fun_name name database expected =
+    fun_name >:: fun _ ->
+    assert_equal (PersonDB.get_table name database) expected
+
+  let test_get_reference fun_name ent database expected =
+    fun_name >:: fun _ ->
+    assert_equal (PersonDB.get_reference ent database) expected
+
+  let test_add_entry fun_name table_name new_row database expected =
+    fun_name >:: fun _ ->
+    assert_equal (PersonDB.add_entry table_name new_row database) expected
+
+  let test_add_entry_exception fun_name table_name new_row database expected =
+    fun_name >:: fun _ ->
+    assert_raises expected (fun () ->
+        PersonDB.add_entry table_name new_row database)
+
+  let test_get_reference_exception fun_name ent database expected =
+    fun_name >:: fun _ ->
+    assert_raises expected (fun () -> PersonDB.get_reference ent database)
+
+  let test_add_named_entry fun_name table_name new_row database expected =
+    fun_name >:: fun _ ->
+    assert_equal (PersonDB.add_named_entry table_name new_row database) expected
+
+  let test_add_named_entry_exception fun_name table_name new_row database
+      expected =
+    fun_name >:: fun _ ->
+    assert_raises expected (fun () ->
+        PersonDB.add_named_entry table_name new_row database)
+
+  let database_person_ent_list = [ Type ("name", Strings); Type ("age", Ints) ]
+
+  let database_airport_ent_list =
     [
-      ( true,
-        "def requires type name",
-        "def ",
-        get_response "err_defn_needs_type_name" );
-      ( true,
-        "def requires ID name",
-        "def Person ",
-        get_response "err_defn_needs_ID_name" );
-      (true, "def test no error", "def Person Name", "|    ");
-      ( false,
-        "wrong type error",
-        "flat float",
-        get_response "err_defn_invalid_type" );
-      (false, "wrong type error", "float   ", get_response "err_defn_no_name");
-    ] )
+      Type ("location", Strings);
+      Type ("passengers", Strings);
+      Type ("delayed", Bools);
+    ]
 
-let pre_defn_assign_tests =
-  ( [ "def Person Name"; "int age"; "float bank"; ""; "assign Person John" ],
+  let t = T.empty [ Type ("name", Strings) ]
+  let new_table = T.empty [ Type ("name", Strings); Type ("age", Ints) ]
+  let elist = [ ("name", String "John"); ("age", Int 25) ]
+  let add_table = T.insert_named new_table elist
+  let reference = ([ String "name" ], Some [ Some (String "Person") ])
+
+  let table2 =
+    T.empty
+      [
+        Type ("location", Strings);
+        Type ("passengers", Strings);
+        Type ("delayed", Bools);
+      ]
+
+  let empty = PersonDB.empty
+  let add_table_database = PersonDB.add_table empty add_table "Person"
+  let person_database = PersonDB.add_table empty new_table "Person"
+  let airport_database = PersonDB.add_table empty table2 "Airport"
+  let large_database = PersonDB.add_table person_database table2 "Airport"
+  let d = PersonDB.db_to_string empty
+  let test_d fun_name = fun_name >:: fun _ -> print_endline d
+
+  let database_tests =
     [
-      (true, "can define types out of order bank", "bank = 5.4", "|    ");
-      (false, "can define types out of order age", "age = 23", "|    ");
-      (false, "can define types out of order end", "", "|    <|\n|> ");
-      ( true,
-        "entry doesnt exist error",
-        "test = 5",
-        get_response "err_create_field_DNE" );
-      ( true,
-        "entry no value error",
-        "age = ",
-        get_response "err_create_field_no_value" );
-      ( true,
-        "entry wrong type error",
-        "age = hello",
-        get_response "err_create_field_wrong_type" );
-    ] )
+      test_empty_database "empty test" empty;
+      test_add_table "Testing add_table" empty new_table "Person"
+        person_database;
+      test_add_table "Testing add_table to non-empty database" person_database
+        table2 "Airport" large_database;
+      test_add_table_exception
+        "Raising failure by adding existing table to database" person_database
+        new_table "Person" PersonDB.TableExists;
+      test_drop_table "Testing drop_table" "Person" person_database empty;
+      test_drop_table "Testing drop_table on empty table" "Person" empty empty;
+      test_drop_table "Testing drop_table with invalid table" "Invalid"
+        person_database person_database;
+      test_drop_table "Testing drop_table on multi-table database" "Person"
+        large_database airport_database;
+      test_get_table "Testing Valid get_table" "Person" person_database
+        (Some new_table);
+      test_get_table "Testing Empty table for get_table" "Person" empty None;
+      test_get_table "Testing Invalid table for get_table" "Airport"
+        person_database None;
+      test_get_table "Testing Valid get_table in multitable database" "Airport"
+        large_database (Some table2);
+      test_build_table "Testing Valid build_table on empty database" empty
+        database_person_ent_list "Person" person_database;
+      test_build_table "Testing Valid build_table on existing database"
+        person_database database_airport_ent_list "Airport" large_database;
+      test_build_table_exception "Testing TableExists exception on build_table"
+        person_database database_person_ent_list "Person" PersonDB.TableExists;
+      test_add_named_entry "Testing add_named_entry" "Person"
+        [ ("name", String "location") ]
+        person_database ();
+      test_add_named_entry_exception "Testing Not_found in add_named_entry"
+        "Invalid"
+        [ ("name", String "location") ]
+        person_database Not_found;
+      test_get_reference "Test Get reference"
+        (Id ("Person", String "John"))
+        add_table_database
+        ( [ Type ("name", Strings); Type ("age", Ints) ],
+          Some [ Some (String "John"); Some (Int 25) ] );
+    ]
+end
 
-let misc_tests =
-  ( [],
-    [ (true, "help message is correct", "help", get_response "help_message") ]
-  )
-
-let rec gather_tests tests =
-  match tests with
-  | [] -> []
-  | (primer, tests) :: t -> make_test primer tests :: gather_tests t
+module DatabaseListTable = DatabaseTests (ListTable)
+module DatabaseHashTable = DatabaseTests (HashTable)
 
 let suite =
-  "search test suite"
-  >::: ([ def_tests; misc_tests; pre_defn_assign_tests ]
-       |> gather_tests |> List.flatten)
+  "test suite"
+  >::: List.flatten
+         [
+           ListTableTests.table_tests;
+           HashTableTests.table_tests;
+           DatabaseListTable.database_tests;
+           DatabaseHashTable.database_tests;
+         ]
 
 let _ = run_test_tt_main suite
